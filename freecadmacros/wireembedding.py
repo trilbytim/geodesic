4f5a9b6ca77bb4474ff528b318ea75755d0bb463
Julian Todd <julian@goatchurch.org.uk>
2023-03-09 17:56:01 +0000

    working on linking up a sequence of triangles and edges defined by the drive curve

--- freecadmacros/wireembedding.py
+++ freecadmacros/wireembedding.py
@@ -57,6 +57,48 @@ class UsefulBoxedTriangleMesh:
                     self.hitreg[i] = self.nhitreg
         return barclosest, barclosestlam
 
+def TriangleNodeOpposite(bar, bGoRight):
+    bartop = bar.GetForeRightBL(bGoRight)
+    if bartop != None:
+        return bartop.GetNodeFore(bartop.nodeback == bar.GetNodeFore(bGoRight))
+    return None
+
+def BarCrossTowards(bar, lam, ebar, elam):
+    assert bar != ebar
+    bpt = Along(lam, bar.nodeback.p, bar.nodefore.p)
+    ept = Along(elam, ebar.nodeback.p, ebar.nodefore.p)
+    axD = P3.ZNorm(ept - bpt)
+    axP = P3.Cross(axD, bar.nodefore.p - bar.nodeback.p)
+    axA = P3.Cross(axP, axD)
+    print("RS ", P3.Dot(axA, bar.nodefore.p - bar.nodeback.p))
+    return ebar, elam
+
+    barforeright = bar.barforeright
+    noderight = barforeright.GetNodeFore(barforeright.nodeback == bar.nodefore)
+    barbackright = barforeright.GetForeRightBL(barforeright.nodeback == bar.nodefore)
+    barbackleft = bar.barbackleft
+    nodeleft = barbackleft.GetNodeFore(barbackleft.nodeback == bar.nodeback)
+    barforeleft = barbackleft.GetForeRightBL(barbackleft.nodeback == bar.nodeback)
+    if ebar in [ bar, barforeright, barbackright, barforeleft, barbackleft ]:
+        return ebar, elam
+
+    axB = P3.ZNorm(bar.nodefore.p - bar.nodeback.p)
+    axP = P3.ZNorm(P3.Cross(noderight.p - nodeleft.p, axB))
+    axR = P3.Cross(axB, axP)
+
+    dvec = ept - bpt
+    return ebar, elam
+
+def BetweenBarListE(barlam, ebarlam):
+    res = [ ]
+    while barlam[0] != ebarlam[0]:
+        barlam = BarCrossTowards(barlam[0], barlam[1], ebarlam[0], ebarlam[1])
+        res.append(barlam)
+        if len(res) > 10:
+            print("failed to generate between bar list")
+            break
+    return res
+    
     
 meshobject = None
 drivepts = None
@@ -74,12 +116,21 @@ print(drivepts[0], drivepts[-1])
 utbm = UsefulBoxedTriangleMesh(meshobject)
 print(utbm)
 
+startbarlam = utbm.FindClosestEdge(P3(*drivepts[0]))
+drivebars = [ startbarlam ]
+for pt in drivepts[1:-1]:
+    ebarlam = utbm.FindClosestEdge(P3(*pt))
+    drivebars.extend(BetweenBarListE(drivebars[-1], ebarlam))
+drivebars.extend(BetweenBarListE(drivebars[-1], startbarlam))
+
 epts = [ ]
-for pt in drivepts:
-    bar, lam = utbm.FindClosestEdge(P3(*pt))
+for bar, lam in drivebars:
     ept = Along(lam, bar.nodeback.p, bar.nodefore.p)
     epts.append(ept)
-Part.show(Part.makePolygon(epts))
+#Part.show(Part.makePolygon(epts))
 
+facets = [ [ Vector(*bar.nodeback.p), Vector(*bar.nodefore.p), Vector(*TriangleNodeOpposite(bar, True).p) ]  for bar, lam in drivebars ]
+mesh = doc.addObject("Mesh::Feature", "m1")
+mesh.Mesh = Mesh.Mesh(facets)
 
 
