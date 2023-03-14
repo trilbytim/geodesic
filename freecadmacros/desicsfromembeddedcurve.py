50ef6ea9826e6efc653453978c75d45b3f2a3b9f
Julian Todd <julian@goatchurch.org.uk>
2023-03-14 16:26:56 +0000

    we can now find the intersection point back in the triangle

--- freecadmacros/desicsfromembeddedcurve.py
+++ freecadmacros/desicsfromembeddedcurve.py
@@ -53,9 +53,9 @@ def TriangleExitCrossCutPlaneRight(tbar, perpvec, perpvecDot):
 
     if dpvdnodeback <= perpvecDot <= dpvdnode2:
         tbarlam = InvAlong(perpvecDot, dpvdnodeback, dpvdnode2)
-        barbackleft = tbar.barforeright.GetForeRightBL(tbar.barforeright.nodeback == tbar.nodefore)
-        assert barbackleft.nodeback.i < barbackleft.nodefore.i
-        return barbackleft, tbarlam, True
+        barbackright = tbar.barforeright.GetForeRightBL(tbar.barforeright.nodeback == tbar.nodefore)
+        assert barbackright.nodeback.i < barbackright.nodefore.i
+        return barbackright, tbarlam, True
 
     if dpvdnode2 <= perpvecDot <= dpvdnodefore:
         bforerightFore = (node2.i < tbar.nodefore.i)
@@ -72,8 +72,7 @@ startbar, startlam = utbm.FindClosestEdge(drivept0)
 drivebars = planecutembeddedcurve(startbar, startlam, driveperpvec)
 # showdrivebarsmesh(drivebars, doc, meshname="m1"):
 # showdrivebarscurve(drivebars, doc)
-
-tridrivebarsmap = dict((drivebars[dseg][0].i, dseg)  for dseg in range(len(drivebars)))
+tridrivebarsmap = dict((facetbetweenbars(drivebars[dseg][0], drivebars[dseg+1][0]).i, dseg)  for dseg in range(len(drivebars)-1))
 
 def drivepointstartfromangle(drivebars, dseg, dlam, dangle):
     tbar = facetbetweenbars(drivebars[dseg][0], drivebars[dseg+1][0])
@@ -86,13 +85,67 @@ def drivepointstartfromangle(drivebars, dseg, dlam, dangle):
     bar, lam, bGoRight = TriangleExitCrossCutPlaneRight(tbar, perpvec, perpvecDot)
     return pt, bar, lam, bGoRight
 
+def triclambarlam(tbar, barlam):
+    bar, lam = barlam
+    if bar == tbar:
+        return lam
+    if bar == tbar.barforeright:
+        return 1.0 + (lam if tbar.barforeright.nodeback == tbar.nodefore else 1.0 - lam)
+    assert bar == tbar.barforeright.GetForeRightBL(tbar.barforeright.nodeback == tbar.nodefore)
+    return 2.0 + (1.0 - lam)
 
-
+def trilamrightangleproj(clam):
+    if clam <= 1.0:
+        return P2(0.0, clam)
+    if clam <= 2.0:
+        return P2(clam - 1.0, 2.0 - clam)
+    return P2(3.0 - clam, 0.0)
+    
+def trilinecrossing(tbar, barlamA0, barlamA1, barlamB0, barlamB1):
+    clamA0 = triclambarlam(tbar, barlamA0)
+    clamA1 = triclambarlam(tbar, barlamA1)
+    clamB0 = triclambarlam(tbar, barlamB0)
+    clamB1 = triclambarlam(tbar, barlamB1)
+    cseq = [ (clamA0, 0), (clamA1, 0), (clamB0, 1), (clamB1, 1) ]
+    cseq.sort()
+    if cseq[0][1] == cseq[1][1] or cseq[1][1] == cseq[2][1]:
+        return -1.0
+    # project clam values into simple right angle triangle, which is a linear transform of the real triangle, so the parametrized intersection position remains the same
+    tA0 = trilamrightangleproj(clamA0)
+    tA1 = trilamrightangleproj(clamA1)
+    tB0 = trilamrightangleproj(clamB0)
+    tB1 = trilamrightangleproj(clamB1)
+    vBperp = P2.CPerp(tB1 - tB0)
+    vBperpdot = P2.Dot(vBperp, tB0)
+    vBperpdotA0 = P2.Dot(vBperp, tA0)
+    vBperpdotA1 = P2.Dot(vBperp, tA1)
+    lamA = (vBperpdot - vBperpdotA0)/(vBperpdotA1 - vBperpdotA0)
+    assert 0.0 <= lamA <= 1.0, lamA
+    DptA0 = Along(barlamA0[1], barlamA0[0].nodeback.p, barlamA0[0].nodefore.p)
+    DptA1 = Along(barlamA1[1], barlamA1[0].nodeback.p, barlamA1[0].nodefore.p)
+    DptB0 = Along(barlamB0[1], barlamB0[0].nodeback.p, barlamB0[0].nodefore.p)
+    DptB1 = Along(barlamB1[1], barlamB1[0].nodeback.p, barlamB1[0].nodefore.p)
+    DptcrossA = Along(lamA, DptA0, DptA1)
+    DvB = DptB1 - DptB0
+    DlamB = P3.Dot(DptcrossA - DptB0, DvB)/DvB.Lensq()
+    DptcrossB = Along(DlamB, DptB0, DptB1)
+    assert (DptcrossB - DptcrossA).Len() < 0.001
+    return lamA   
+    
+def drivecurveintersectionfinder(drivebars, tridrivebarsmap, barlamB0, barlamB1):
+    tbar = facetbetweenbars(barlamB0[0], barlamB1[0])
+    if tbar.i not in tridrivebarsmap:
+        return -1, -1.0
+    dseg = tridrivebarsmap[tbar.i]
+    Dtbar = facetbetweenbars(drivebars[dseg][0], drivebars[dseg+1][0])
+    assert tbar == Dtbar
+    dlam = trilinecrossing(tbar, drivebars[dseg], drivebars[dseg+1], barlamB0, barlamB1)
+    if dlam == -1.0:
+        return -1, -1.0
+    return dseg, dlam
 
 N = 5
 startangtoline = 30
-vang = P2(math.cos(math.radians(startangtoline)), math.sin(math.radians(startangtoline)))
-print("vang", vang)
 pts = [ Along(lam, bar.nodeback.p, bar.nodefore.p)  for bar, lam in drivebars ]
 ptcls = cumlengthlist(pts)
 for i in range(N):
@@ -112,9 +165,10 @@ for i in range(N):
         ptprev = ptcurr
         ptcurr = Along(lam, bar.nodeback.p, bar.nodefore.p)
         assert (ptprev - Dc).Len() < 0.001
-        tbar = facetbetweenbars(prevbar, bar)
-        print(tbar, prevbar.GetForeRightBL(prevbGoRight), bar.GetForeRightBL(not bGoRight))
-        if tbar.i in tridrivebarsmap:
+        dcseg, dclam = drivecurveintersectionfinder(drivebars, tridrivebarsmap, (prevbar, prevlam), (bar, lam))
+        if dcseg != -1:
+            cpt = Along(dclam, pts[dcseg], pts[dcseg+1])
+            gpts.append(cpt)
             break
         gpts.append(ptcurr)
     Part.show(Part.makePolygon([Vector(*p)  for p in gpts]))
