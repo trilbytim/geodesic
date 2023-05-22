4de2bb681c11cf495542e2de82777cdef105ec45
Julian Todd <julian@goatchurch.org.uk>
2023-05-22 20:12:36 +0100

    separating the switchback sections into blocks and handling backside of eyelet

--- freecadmacros/gui_postprocesswire.py
+++ freecadmacros/gui_postprocesswire.py
@@ -39,40 +39,6 @@ freecadutils.init(App)
 # E3 - Rotation both chucks (headstock and tailstock)
 # E4 - Rotation of tailstock relative to headstock (rarely used)
 
-def outputsrclines(pts, freetapelength, robotstate):
-    prevE1 = robotstate["E1"]  # used to track the number of complete winds on the eyelet part
-    prevE3 = robotstate["E3"]  # used to track the number of complete winds on the lathe part
-    res = [ ]
-
-    # loop through the points and vectors 
-    # (we use the incoming vector at each triangle edge, not the outgoing vector)
-    for i in range(1, len(pts)):
-        pt = pts[i]
-        vec = P3.ZNorm(pts[i] - pts[i-1])
-        tapevector = vec*freetapelength
-        tcp = pt + tapevector
-        radialvec = P2(tcp.x, tcp.z)
-        lE3 = radialvec.Arg()
-        E3 = lE3 + 360*int((abs(prevE3 - lE3)+180)/360)*(1 if prevE3 > lE3 else -1)
-        X = -radialvec.Len()
-        tangentunitvec = -P2.CPerp(radialvec)*(1/X)
-        E1vec = P2(-P2.Dot(tangentunitvec, P2(tapevector.x, tapevector.z)), -tapevector.y)
-        lE1 = E1vec.Arg()
-        E1 = lE1 + 360*int((abs(prevE1 - lE1)+180)/360)*(1 if prevE1 > lE1 else -1)
-        res.append({"X":X, "Y":tcp.y, "E1":E1, "E3":E3})
-        prevE1 = E1
-        prevE3 = E3
-        robotstate["Y"] = tcp.y
-        robotstate["X"] = X
-    robotstate["E1"] = prevE1
-    robotstate["E3"] = prevE3
-
-    return res
-
-def srcpt(ps):
-	return "{%s}" % ", ".join("%s %+9.3f" % (c, ps[c])  for c in ["X", "Y", "Z", "A", "E1", "E3", "E4"]  if c in ps)
-
-
 
 def projectToXvalplane(pt, vec, cx):
     qa = P2(vec.x, vec.z).Lensq()
@@ -88,14 +54,12 @@ def projectToXvalplane(pt, vec, cx):
     return res
 
 class TCPplusfibre:
-    def __init__(self, tcpR, ptR, prevtcp):
+    def __init__(self, tcpR, ptR):
         vecR = ptR - tcpR
         self.freefibrelength = vecR.Len()
         self.DvecR = vecR
         
-        lE3 = P2(-tcpR.x, -tcpR.z).Arg()
-        prevE3 = prevtcp.E3 if prevtcp is not None else lE3
-        self.E3 = lE3 + 360*int((abs(prevE3 - lE3)+180)/360)*(1 if prevE3 > lE3 else -1)
+        self.E3 = P2(-tcpR.x, -tcpR.z).Arg()
         
         tcpRd = P2(tcpR.x, tcpR.z).Len()
         rvX = P3(-tcpR.x/tcpRd, 0, -tcpR.z/tcpRd)
@@ -111,9 +75,12 @@ class TCPplusfibre:
         TOL_ZERO(self.freefibrelength - vec.Len())
         TOL_ZERO((self.RotByE3(vec) - vecR).Len(), ("rotByE3vec failed"))
 
-
         self.E1 = P2(vec.z, vec.y).Arg()
         self.E2 = math.degrees(math.acos(vec.x/self.freefibrelength))
+        if vec.z < 0.0:
+            self.E1 = 180 + self.E1
+            self.E2 = -self.E2
+            
         TOL_ZERO((tcpR - self.GetTCP(True)).Len())
         TOL_ZERO((vecR - self.GetVecR(True)).Len(), (vecR, self.GetVecR(True)))
 
@@ -134,36 +101,48 @@ class TCPplusfibre:
         res = P3(cosE2, sinE2*sinE1, sinE2*cosE1)*self.freefibrelength
         return self.RotByE3(res) if bRotated else res
 
+    def applyE3Winding(self, prevE3):
+        lE3 = self.E3
+        self.E3 = lE3 + 360*int((abs(prevE3 - lE3)+180)/360)*(1 if prevE3 > lE3 else -1)
+
+    def applyE1Winding(self, prevE1):
+        lE1 = self.E1
+        self.E1 = lE1 + 360*int((abs(prevE1 - lE1)+180)/360)*(1 if prevE1 > lE1 else -1)
+
+SRCparameters = ["X", "Y", "Z", "A", "E1", "E3", "E4"]
+def srcpt(ps):
+    return "{%s}" % ", ".join("%s %+9.3f" % (c, ps[c])  for c in SRCparameters  if c in ps)
 
 def okaypressed():
     print("Okay Pressed") 
+    if qoptionsrcdebug.isChecked():
+        SRCparameters.extend(["E2", "fleng"])
+    
     toolpathobject = freecadutils.findobjectbylabel(qtoolpath.text())
+    tcpconstXval = float(qxconst.text())
+    textlen = float(qtoolpathlength.text()) if len(qtoolpathlength.text()) != 0 and qtoolpathlength.text()[-1] != " " else None
 
     tapecurve = [ P3(p.X, p.Y, p.Z)  for p in toolpathobject.Shape.Vertexes ]
-    tcpconstXval = float(qxconst.text())
-
-    textlen = float(qtoolpathlength.text()) if len(qtoolpathlength.text()) != 0 and qtoolpathlength.text()[-1] != " " else None
     
     tcps = [ ]
     for i in range(len(tapecurve)):
         vecNout = P3.ZNorm(tapecurve[max(i,1)] - tapecurve[max(i,1)-1])
         ptR = tapecurve[i]
         tcpR = projectToXvalplane(ptR, vecNout, abs(tcpconstXval))
-        prevtcp = tcps[-1] if len(tcps) != 0 else None
-        tcp = TCPplusfibre(tcpR, ptR, prevtcp)
-        tgcpmov = (prevtcp.GetTCP(True) - tcp.GetTCP(True)).Len() if prevtcp != None else 1000
-        if tgcpmov < 0.05:
-            print("Skipping trivial aligned tcp motion", i, tgcpmov)
-        else:
-            tcps.append(tcp)
+        tcp = TCPplusfibre(tcpR, ptR)
+        tcps.append(tcp)
+        if len(tcps) >= 2:
+            prevtcp = tcps[-2]
+            tcp.applyE3Winding(prevtcp.E3)
+            tcp.applyE1Winding(prevtcp.E1)
+            tgcpmov = (prevtcp.GetTCP(True) - tcp.GetTCP(True)).Len()
+            if tgcpmov < 0.05:
+                print("Skipping trivial linear aligned tcp motion", i, tgcpmov)
+                tcps.pop()
         if i != 0 and textlen is not None:
             textlen -= (tapecurve[i] - tapecurve[i-1]).Len()
             if textlen <= 0:
                 break
-    
-    foutputsrc = qoutputsrcfile.text()
-    headersrc = os.path.join(os.path.split(__file__)[0], "header.src")
-    print("outputting src toolpath ", os.path.abspath(foutputsrc))
 
     tcpblocks = [ [ tcps[0] ] ]
     for itcp in range(1, len(tcps)):
@@ -171,53 +150,89 @@ def okaypressed():
         tcpprev = tcpblocks[-1][-1]
         Ydirectionchange = (len(tcpblocks[-1]) >= 2 and ((tcpblocks[-1][-2].Y < tcpblocks[-1][-1].Y) != (tcpblocks[-1][-1].Y < tcp.Y)))
         Yhardswitchback = P3.Dot(P3.ZNorm(tcpprev.GetVecR(True)), P3.ZNorm(tcp.GetVecR(True))) < -0.5
-        if Yhardswitchback:
-            print("Yhardswitchback", itcp, Ydirectionchange)
-            assert Ydirectionchange, "hardswitchback should coincide with a Ydirection change"
         if Ydirectionchange:
-            tcpblocks.append([ tcpprev ])
+            if not Yhardswitchback:
+                tcpblocks.append([ tcpprev ])
+            else:
+                tcpblocks.append([ ])
+        else:
+            assert not Yhardswitchback, "hardswitchback should be a Ydirectionchange"
         tcpblocks[-1].append(tcp)
-            
+
+    tcpblockYdirection = [ ]
+    tcpblockE3direction = [ ]
+    tcpblockstartwithswitchback = [ ]
+    for i in range(len(tcpblocks)):
+        tcpblock = tcpblocks[i]
+        tcpblockYdirection.append(1 if tcpblock[0].Y < tcpblock[-1].Y else -1)
+        tcpblockE3direction.append(1 if tcpblock[0].E3 < tcpblock[-1].E3 else -1)
+        Yhardswitchback = 0
+        if i != 0:
+            tcpprev = tcpblocks[i-1][-1]
+            tcpcurr = tcpblock[0]
+            backvecdot = P3.Dot(P3.ZNorm(tcpprev.GetVecR(True)), P3.ZNorm(tcpcurr.GetVecR(True)))
+            #print("backvecdot", backvecdot, tcpprev.Y, tcpcurr.Y)
+            if backvecdot < -0.5:
+                print("Yhardswitchback block", i, "to Y direction", tcpblockYdirection[i], "spin", tcpblockE3direction[i])
+                Yhardswitchback = 1 if tcpblockE3direction[i] else -1
+        tcpblockstartwithswitchback.append(Yhardswitchback)
+
+    print(tcpblockYdirection)
+    print(tcpblockE3direction)
+    print(tcpblockstartwithswitchback)
+    
+    tcpblockslinked = [ ]
+    tcpblockslinkedstarthalt = [ ]
+    for i in range(len(tcpblocks)):
+        tcpblock = tcpblocks[i]
+        if tcpblockstartwithswitchback[i] != 0:
+            tcpblockslinked.append([ tcpblocks[i-1][-1], tcpblock[0] ])
+            print("This should be the linking motion put in here")
+            tcpblockslinkedstarthalt.append(tcpblockstartwithswitchback[i])
+        tcpblockslinked.append(tcpblock)
+        tcpblockslinkedstarthalt.append(0)
+    
+    foutputsrc = qoutputsrcfile.text()
+    headersrc = os.path.join(os.path.split(__file__)[0], "header.src")
+    print("outputting src toolpath ", os.path.abspath(foutputsrc))
+
     if len(qoutputsweepmesh.text()) != 0 and qoutputsweepmesh.text()[-1] != "*":
         facets = [ ]
-        for i in range(itcp-1):
-            tcp0, tcp1 = tcps[i].GetTCP(True), tcps[i+1].GetTCP(True)
-            vecr0, vecr1 = tcps[i].GetVecR(True), tcps[i+1].GetVecR(True)
-            fp0, fp1 = tcp0 + vecr0, tcp1 + vecr1
-            #if (fp0 - P3(-108.77,750,61.57)).Len() > 6:  continue
-            facets.append([Vector(*tcp0), Vector(*fp0), Vector(*tcp1)])
-            facets.append([Vector(*tcp1), Vector(*fp0), Vector(*fp1)])
+        for i in range(len(tcpblockslinked)):
+            tcpblock = tcpblockslinked[i]
+            if not tcpblockslinkedstarthalt[i]:  continue
+            for j in range(len(tcpblock)-1):
+                tcp0, tcp1 = tcpblock[j].GetTCP(True), tcpblock[j+1].GetTCP(True)
+                vecr0, vecr1 = tcpblock[j].GetVecR(True), tcpblock[j+1].GetVecR(True)
+                fp0, fp1 = tcp0 + vecr0, tcp1 + vecr1
+                #if (fp0 - P3(-108.77,750,61.57)).Len() > 6:  continue
+                facets.append([Vector(*tcp0), Vector(*fp0), Vector(*tcp1)])
+                facets.append([Vector(*tcp1), Vector(*fp0), Vector(*fp1)])
         mesh = freecadutils.doc.addObject("Mesh::Feature", qoutputsweepmesh.text())
         mesh.ViewObject.Lighting = "Two side"
         mesh.ViewObject.DisplayMode = "Flat Lines"
         mesh.Mesh = Mesh.Mesh(facets)
 
-    print("blocks ", list(map(len, tcpblocks)))
-
-    def srcpt(ps):
-        return "{%s}" % ", ".join("%s %+9.3f" % (c, ps[c])  for c in ["X", "Y", "Z", "A", "E1", "E3", "E4"]  if c in ps)
+    print("blocks ", list(map(len, tcpblockslinked)))
 
     Ymid = 1000
     def srctcp(tcp):
-        return srcpt({"X":tcp.X, "Y":tcp.Y + Ymid, "E1":tcp.E1, "E3":tcp.E3*1000/360})
+        return srcpt({"X":tcp.X, "Y":tcp.Y + Ymid, "E1":tcp.E1, "E2":tcp.E2, "E3":tcp.E3*1000/360, "fleng":tcp.freefibrelength})
 
     headersrc = os.path.join(os.path.split(__file__)[0], "header.src")
     print("making toolpath: ", os.path.abspath(foutputsrc))
     fout = open(foutputsrc, "w")
     fout.write(open(headersrc).read())
     fout.write("SLIN %s\n" % srcpt({"X":-200, "Y":Ymid, "Z":0, "A":0, "E1":0, "E3":0, "E4":0}))
-    for i in range(len(tcpblocks)):
-        tcpblock = tcpblocks[i]
+    for i in range(len(tcpblockslinked)):
+        tcpblock = tcpblockslinked[i]
         if i == 0:
             fout.write("\nSLIN %s\n" % srctcp(tcpblock[0]))
             fout.write("HALT\n")
-        else:
-            Yhardswitchback = P3.Dot(P3.ZNorm(tcpblock[0].GetVecR(True)), P3.ZNorm(tcpblock[1].GetVecR(True))) < -0.5
-            if Yhardswitchback:
-                fout.write("HALT  ; switchback \n")
-                fout.write("SLIN %s\n\n" % srctcp(tcpblock[1]))
+        elif tcpblockslinkedstarthalt[i]:
+            fout.write("HALT  ; switchback %s\n" % ("up" if tcpblockslinkedstarthalt[i] == 1 else "down"))
         fout.write("SPLINE\n")
-        for tcp in tcpblock[2 if Yhardswitchback else 1:]:
+        for tcp in tcpblock[1:]:
             fout.write("SPL %s\n" % srctcp(tcp))
         fout.write("ENDSPLINE\n\n")
     fout.write("SLIN %s\n" % srcpt({"X":-200, "Y":Ymid, "E1":0}))
@@ -241,6 +256,11 @@ QtCore.QObject.connect(okButton, QtCore.SIGNAL("pressed()"), okaypressed)
 qtoolpath.setText(freecadutils.getlabelofselectedwire())
 qxconst = freecadutils.qrow(qw, "xconst: ", 15+35*2, "-115")
 qtoolpathlength = freecadutils.qrow(qw, "(Length): ", 15+35*1, "0 ", 260)
+
+qoptionsrcdebug = QtGui.QCheckBox("Dbg SRC params", qw)
+qoptionsrcdebug.move(80+260, 15+35*3)
+#qoptionsrcdebug.setChecked(True)
+
 
 toolpathobject = freecadutils.findobjectbylabel(qtoolpath.text())
 if toolpathobject:
