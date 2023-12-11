ba68cd7444e86022f26f974d56b3397de20fa653
Julian Todd <julian@goatchurch.org.uk>
2023-12-11 16:41:03 +0000

    make new genconstthickfromsplaytask

--- freecadmacros/gui_genconstthickfromplaytask.py
+++ freecadmacros/gui_genconstthickfromplaytask.py
@@ -51,6 +51,13 @@ def sgetlabelofselectedsketch(sel):
         if hasattr(s, "Module") and s.Module == 'Sketcher':
             return s.Label
     return ""
+
+def sgetlabelofselectedgroupwithproperties(sel, properties):
+    for s in sel:
+        if isinstance(s, App.DocumentObjectGroup) and set(properties).issubset(s.PropertiesList):
+            return s.Label
+    return ""
+
     
 def sfindobjectbylabel(doc, lab):
     objs = [ obj  for obj in doc.findObjects(Label=lab)  if obj.Label == lab ]
@@ -61,52 +68,6 @@ mandrelradius = 110  # fc6 file
 LRdirection = 1
 appaturepapproachpoint = P3(0,-150,0)
 maxlength = 2500
-
-def findappatureclosestapproach(gbs, sphpt):
-    rclose = 0
-    iclose = -1
-    lamclose = -1.0
-    for i in range(1, len(gbs) - 2):
-        p0 = gbs[i].pt
-        p1 = gbs[i+1].pt
-        v0 = p0 - sphpt
-        v0len = v0.Len()
-        if iclose == -1 or v0len < rclose:
-            rclose = v0len
-            iclose = i
-            lamclose = 0.0
-        if v0len > rclose + 10.0:
-            continue
-        v = p1 - p0
-        vlensq = v.Lensq()
-        if vlensq != 0:
-            lam = -P3.Dot(v0, v) / v.Lensq()
-            if 0.0 < lam < 1.0:
-                vd = v0 + v*lam
-                TOL_ZERO(P3.Dot(vd, v))
-                vdlen = vd.Len()
-                if vdlen < rclose:
-                    rclose = vdlen
-                    iclose = i
-                    lamclose = lam
-    gbt = GBarT([(gbs[iclose].bar, gbs[iclose].lam), (gbs[iclose+1].bar, gbs[iclose+1].lam)], 0, lamclose)
-    TOL_ZERO((gbt.pt - sphpt).Len() - rclose)
-    return gbt, rclose
-
-def spherecutlam(p0, p1, sphpt, sphrad):
-    v = p1 - p0
-    vs = p0 - sphpt
-    qa = v.Lensq()
-    qb2 = P3.Dot(vs, v)
-    qc = vs.Lensq() - sphrad*sphrad
-    qdq = qb2*qb2 - qa*qc
-    qs = math.sqrt(qdq) / qa
-    qm = -qb2 / qa
-    q = qm + qs
-    TOL_ZERO(qa*q*q + qb2*2*q + qc)
-    Dpt = vs + v*q
-    assert (-0.01 <= q <= 1.01), q
-    return q
 
 
 def TriangleCrossSphereRight(tbar, bGoRight, sphpt, sphrad, bContinuation):
@@ -197,14 +158,10 @@ def makesplaycycle(gbs, sphpt):
             break
     return sphbars, sphrad
     
-def setpropertyval(obj, atype, name, value):
-    if name not in obj.PropertiesList:
-        obj.addProperty(atype, name, "filwind")
-    setattr(obj, name, value)
 
-class DirectedSplayTaskPanel(QtGui.QWidget):
+class GenConstThickFromSplayTaskPanel(QtGui.QWidget):
     def __init__(self):
-        x = os.path.join(os.path.split(__file__)[0], "directedsplaytask.ui")
+        x = os.path.join(os.path.split(__file__)[0], "genconstthickfromsplaytask.ui")
         self.form = FreeCADGui.PySideUic.loadUi(x)
         self.form.setMinimumSize(0, 300)
         QtCore.QObject.connect(self.form.pushButton, QtCore.SIGNAL("pressed()"), self.pushbutton)  
@@ -213,28 +170,33 @@ class DirectedSplayTaskPanel(QtGui.QWidget):
     def update(self):
         self.doc = App.ActiveDocument
         self.sel = App.Gui.Selection.getSelection()
-        self.form.qmeshobject.setText(sgetlabelofselectedmesh(self.sel))
-        self.form.qsketchplane.setText(sgetlabelofselectedsketch(self.sel))
+        splaycircles = sgetlabelofselectedgroupwithproperties(self.sel, ["sketchplane", "meshobject"])
+        if splaycircles:
+            self.form.qsplaycircles.setText(splaycircles)
+        splaycirclesgroup = sfindobjectbylabel(self.doc, self.form.qsplaycircles.text())
+        if splaycirclesgroup:
+            self.form.qsketchplane.setText(splaycirclesgroup.sketchplane)
+            self.form.qmeshobject.setText(splaycirclesgroup.meshobject)
+            self.form.qalongwire.setValue(splaycirclesgroup.alongwire)
 
     def pushbutton(self):
         print("Pushbutton pressed")
 
     def apply(self):
         print("apply!!")
+        splaycirclefolder = sfindobjectbylabel(self.doc, self.form.qsplaycircles.text())
         sketchplane = sfindobjectbylabel(self.doc, self.form.qsketchplane.text())
         meshobject = sfindobjectbylabel(self.doc, self.form.qmeshobject.text())
-        splaycircles = self.form.qsplaycircles.text()
-        splayhoops = self.form.qsplayhoops.text()
+        alongwire = float(self.form.qalongwire.text())
+        return
+
         alongwire = float(self.form.qalongwire.text())
         minangle = float(self.form.qminangle.text())
         maxangle = float(self.form.qmaxangle.text())
         anglestep = float(self.form.qanglestep.text())
 
-        splaygroup = freecadutils.getemptyfolder(self.doc, splayhoops)
-        splaycyclegroup = freecadutils.getemptyfolder(self.doc, splaycircles)
-        setpropertyval(splaycyclegroup, "App::PropertyFloat", "alongwire", alongwire)
-        setpropertyval(splaycyclegroup, "App::PropertyString", "sketchplane", sketchplane.Label)
-        setpropertyval(splaycyclegroup, "App::PropertyString", "meshobject", meshobject.Label)
+        splaygroup = freecadutils.getemptyfolder(self.doc, splayfolder)
+        splaycyclegroup = freecadutils.getemptyfolder(self.doc, splaycyclefolder)
 
         utbm = UsefulBoxedTriangleMesh(meshobject.Mesh)
         drivecurve = makedrivecurve(sketchplane, utbm, mandrelradius)
@@ -252,17 +214,17 @@ class DirectedSplayTaskPanel(QtGui.QWidget):
 
             ply = Part.show(Part.makePolygon([Vector(*gb.pt)  for gb in gbs]), name)
             splaygroup.addObject(ply)
-            setpropertyval(ply, "App::PropertyFloat", "alongwire", alongwire)
-            setpropertyval(ply, "App::PropertyAngle", "dsangle", dsangle)
-            setpropertyval(ply, "App::PropertyFloat", "alongwirelanded", alongwirelanded)
-            setpropertyval(ply, "App::PropertyFloat", "angcrosslanded", angcrosslanded)
+            ply.addProperty("App::PropertyFloat", "alongwire", "filwind"); ply.alongwire = alongwire
+            ply.addProperty("App::PropertyAngle", "dsangle", "filwind"); ply.dsangle = dsangle
+            ply.addProperty("App::PropertyFloat", "alongwirelanded", "filwind"); ply.alongwirelanded = alongwirelanded
+            ply.addProperty("App::PropertyFloat", "angcrosslanded", "filwind"); ply.angcrosslanded = angcrosslanded
 
             cply = Part.show(Part.makePolygon([Vector(*gb.pt)  for gb in cgbs]), name)
-            setpropertyval(cply, "App::PropertyFloat", "alongwire", alongwire)
-            setpropertyval(cply, "App::PropertyAngle", "dsangle", dsangle)
-            setpropertyval(cply, "App::PropertyFloat", "alongwirelanded", alongwirelanded)
-            setpropertyval(cply, "App::PropertyFloat", "angcrosslanded", angcrosslanded)
-            setpropertyval(cply, "App::PropertyFloat", "sphrad", sphrad)
+            cply.addProperty("App::PropertyFloat", "alongwire", "filwind"); ply.alongwire = alongwire
+            cply.addProperty("App::PropertyAngle", "dsangle", "filwind"); ply.dsangle = dsangle
+            cply.addProperty("App::PropertyFloat", "alongwirelanded", "filwind"); ply.alongwirelanded = alongwirelanded
+            cply.addProperty("App::PropertyFloat", "angcrosslanded", "filwind"); ply.angcrosslanded = angcrosslanded
+            cply.addProperty("App::PropertyFloat", "sphrad", "filwind"); cply.sphrad = sphrad
             splaycyclegroup.addObject(cply)
 
 
@@ -288,4 +250,4 @@ class DirectedSplayTaskPanel(QtGui.QWidget):
         print("Finish")
         Gui.Control.closeDialog()
 
-Gui.Control.showDialog(DirectedSplayTaskPanel())
+Gui.Control.showDialog(GenConstThickFromSplayTaskPanel())
